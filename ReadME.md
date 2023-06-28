9f9cf6b1018a53efa729e711f071594e990ec92b
venkateswararao10 <veeramreddivenkatesh2003@gmail.com>
2023-06-28 22:26:58 +0530

    firstcommit

---
+++ ReadME.md
@@ -0,0 +1,11 @@
+## End to End ML Project
+## create environment
+```
+conda activate
+conda create -p venv python==3.8
+conda activate venv\
+```
+## install all necessary libraries
+```
+pip install -r requirements.txt
+```
--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask,render_template,request
+from src.pipeline.predictionpipeline import customdata,predictpipeline
+app=Flask(__name__)
+@app.route('/')
+def homepage():
+     return render_template('home.html')
+@app.route('/submit',methods=['POST'])
+def predictpage():
+     if request.method=='POST':
+          cd=customdata(
+               carat=float(request.form.get('carat')),
+               depth = float(request.form.get('depth')),
+            table = float(request.form.get('table')),
+            x = float(request.form.get('x')),
+            y = float(request.form.get('y')),
+            z = float(request.form.get('z')),
+            cut = request.form.get('cut'),
+            color= request.form.get('color'),
+            clarity = request.form.get('clarity')
+          )
+          df=cd.get_data_as_dataframe()
+     obj=predictpipeline()
+     result=obj.predict(df)
+     predprice=round(float(result),2)
+     return render_template('submit.html',predprice=predprice)
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
-
-if __name__=="__main__":
-    app.run(host="0.0.0.0")
+if __name__=='__main__':
+     app.run(host='0.0.0.0',debug=True)
